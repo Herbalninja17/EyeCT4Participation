f5af5386ebead10bf960688702d79e3c0301bf4b
Olaf van Dijk <336692@student.fontys.nl>
2016-04-13 12:02:28 +0200

    implementatie 2e helft

--- EyeCT4Participation/EyeCT4Participation/UI/RequestOverview.cs
+++ EyeCT4Participation/EyeCT4Participation/UI/RequestOverview.cs
@@ -3,10 +3,44 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using EyeCT4Participation.Business;
+using EyeCT4Participation.Business.User;
 
 namespace EyeCT4Participation
 {
-    class hulpvraagOverzicht
+    class RequestOverview
     {
+        public List<Request> requests { get; set; }
+
+        public int needyID { get; set; }
+
+        public int volunteerID { get; set; }
+
+        public RequestOverview(List<Request> requests, int needyID, int volunteerID)
+        {
+            this.requests = requests;
+            this.needyID = needyID;
+            this.volunteerID = volunteerID;
+        }
+
+        public void GetRequestList()
+        {
+            
+        }
+
+        public void PostRequest(Request request)
+        {
+
+        }
+
+        public void DeleteRequest(Request request)
+        {
+
+        }
+
+        public void React(Needy needy, Request request)
+        {
+
+        }
     }
 }
--- EyeCT4Participation/EyeCT4Participation/UI/ReviewOverview.cs
+++ EyeCT4Participation/EyeCT4Participation/UI/ReviewOverview.cs
@@ -3,10 +3,39 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using EyeCT4Participation.Business;
+using EyeCT4Participation.Business.User;
 
 namespace EyeCT4Participation.UI
 {
     class ReviewOverview
     {
+        public List<Review> reviews { get; set; }
+
+        public int needyID { get; set; }
+
+        public int volunteerID { get; set; }
+
+        public ReviewOverview(List<Review> reviews, int needyID, int volunteerID)
+        {
+            this.reviews = reviews;
+            this.needyID = needyID;
+            this.volunteerID = volunteerID;
+        }
+
+        public void WriteReview(Review review)
+        {
+
+        }
+
+        public void DeleteReview(Review review)
+        {
+
+        }
+
+        public void ReadReview(Needy needy)
+        {
+
+        }
     }
 }
