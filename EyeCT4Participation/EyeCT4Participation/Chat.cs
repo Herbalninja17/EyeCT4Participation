65848e3976f7aa3a6295637122ca08541926cb3b
Olaf van Dijk <336692@student.fontys.nl>
2016-04-14 12:13:59 +0200

    testHEHE

--- EyeCT4Participation/EyeCT4Participation/Chat.cs
+++ EyeCT4Participation/EyeCT4Participation/Chat.cs
@@ -7,14 +7,21 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using EyeCT4Participation.UI;
 
 namespace EyeCT4Participation
 {
     public partial class Chat : Form
     {
+        int needyid;
+        int volunteerid;
+        List<string> messages = new List<string>();
+
         public Chat()
         {
             InitializeComponent();
+            Chatbox chat = new Chatbox(volunteerid, needyid, messages);
+            chat.GetChat(needyid, volunteerid);
         }
 
         private void closechatBTN_Click(object sender, EventArgs e)
